511ee6ce42aa0a3f3df9c9d8a8aff4b621842bd9
Adam Bialobrzewski <adam_bialobrzewski@epam.com>
2020-04-07 08:48:08 +0200

    Dockerizing

--- superheroback/hero-service/src/main/java/com/superhero/services/SuperHeroETLService.java
+++ superheroback/hero-service/src/main/java/com/superhero/services/SuperHeroETLService.java
@@ -1,6 +1,5 @@
 package com.superhero.services;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.superhero.persistence.dao.repository.BiographyRepository;
@@ -9,7 +8,7 @@ import com.superhero.persistence.dao.repository.PowerstatsRepository;
 import com.superhero.persistence.domain.Biography;
 import com.superhero.persistence.domain.Hero;
 import com.superhero.persistence.domain.Powerstats;
-import com.superhero.utils.HeroIdUtil;
+import com.superhero.utils.SuperHeroUtil;
 import lombok.extern.apachecommons.CommonsLog;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
@@ -24,10 +23,10 @@ import java.util.Set;
 @CommonsLog
 public class SuperHeroETLService {
 
-    @Value(value = "${super-hero-api-url}")
+    @Value(value = "${super.hero.api.url}")
     private String apiUrl;
 
-    @Value(value = "${super-hero-api-access-token}")
+    @Value(value = "${super.hero.api.access.token}")
     private String apiAccessToken;
 
     @Autowired
@@ -40,7 +39,7 @@ public class SuperHeroETLService {
     BiographyRepository biographyRepository;
 
     @Autowired
-    HeroIdUtil heroIdUtil;
+    SuperHeroUtil superHeroUtil;
 
     @PostConstruct
     public void init(){
@@ -49,15 +48,17 @@ public class SuperHeroETLService {
 
     private void runInNewThread(){
         Runnable task = () -> {
-            Set<Integer> ids = heroIdUtil.getRandomIds();
+            Set<Integer> ids = superHeroUtil.getRandomIds();
             log.info("start Extract Transfer and Load Process  for super heroes by ids: "+ids);
             for(Integer id : ids){
                 try {
                     String result = this.getHeroFromExternalApi(id);
                     Hero hero = this.convertToHero(result);
                     this.saveInDB(hero);
-                } catch (JsonProcessingException e) {
-                    log.error("Can not download super heroes");
+                } catch (Exception e) {
+                    log.error("Can not download super heroes - "+e.getMessage());
+                    e.printStackTrace();
+                    break;
                 }
             }
             log.info("Stop ETL process for super heroes");
@@ -71,7 +72,8 @@ public class SuperHeroETLService {
         return restTemplate.getForObject(uri, String.class);
     }
 
-    private Hero convertToHero(String value) throws JsonProcessingException {
+    private Hero convertToHero(String value) throws Exception {
+        superHeroUtil.checkResponse(value);
         ObjectMapper objectMapper = new ObjectMapper();
         JsonNode jsonNode = objectMapper.readTree(value);
         return Hero.builder()
@@ -82,6 +84,8 @@ public class SuperHeroETLService {
                 .powerstats(objectMapper.readValue(jsonNode.get("powerstats").toString(),Powerstats.class))
                 .build();
     }
+
+
 
     @Transactional
     private Hero saveInDB(Hero hero){
--- superheroback/hero-service/src/main/java/com/superhero/utils/SuperHeroUtil.java
+++ superheroback/hero-service/src/main/java/com/superhero/utils/SuperHeroUtil.java
@@ -1,5 +1,7 @@
 package com.superhero.utils;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.extern.apachecommons.CommonsLog;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
@@ -10,7 +12,7 @@ import java.util.Set;
 
 @Component
 @CommonsLog
-public class HeroIdUtil {
+public class SuperHeroUtil {
 
     @Value(value = "${max-id-value}")
     private int maxIdValue;
@@ -26,5 +28,13 @@ public class HeroIdUtil {
 
     public int getRandomInteger(int maximum, int minimum){
         return ((int) (Math.random()*(maximum - minimum))) + minimum;
+    }
+
+    public void checkResponse(String response) throws Exception {
+        if(response.contains("\"response\":\"error\"")) {
+            ObjectMapper objectMapper = new ObjectMapper();
+            JsonNode jsonNode = objectMapper.readTree(response);
+            throw new Exception(jsonNode.get("error").asText());
+        }
     }
 }
